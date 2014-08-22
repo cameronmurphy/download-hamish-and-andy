3a709d45af5ef17017897d9021d26b17004f58e8
Cameron Murphy <camurphy@gmail.com>
2014-08-22 14:52:45 +1000

    Only strip date from title if it can be found

--- download-hamish-and-andy.py
+++ download-hamish-and-andy.py
@@ -236,7 +236,10 @@ class HamishAndAndyPodcastScrubber():
     @staticmethod
     def cleanup_title(podcast):
         podcast['title'] = podcast['title'].partition('-')[2].strip()
-        podcast['title'] = re.sub(HamishAndAndyPodcastScrubber.NAME_WITH_DATE_REGEX, '\\2', podcast['title'])
+
+        if HamishAndAndyPodcastScrubber.search_and_parse_date(podcast['title']):
+            podcast['title'] = re.sub(HamishAndAndyPodcastScrubber.NAME_WITH_DATE_REGEX, '\\2', podcast['title'])
+
         podcast['title'] = podcast['title'].lstrip(' -(,')
 
         if podcast['title'].find('(') == -1:
